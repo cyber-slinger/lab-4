8f82502a85cb75012c3020fac9ea6d4fe49903d3
cyber-slinger <sndpsbd@gmail.com>
2024-10-04 09:35:14 +1000

    Array of object, loop, checkboxes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import List from './components/List';
 import './App.css';
 
+
+const tasks = [
+    { description: 'Learn React', completed: true },
+    { description: 'Learn JSX', completed: false },
+    { description: 'Build a React App', completed: false }
+    ];
+
 function App() {
     return (
         <div className='page'>     
-            <List heading='My Task 1'/>
+            <List heading='My Task 1' task={tasks}/>
         </div>
     );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,19 +1,26 @@
 function Task(props){
 	return(
-		<li>{props.description}</li>
+		<li>{props.description}<input type="checkbox" checked={props.completed} readOnly/></li>
+		
 
 	);
 }
 
 function List(props) {
+	var taskarray=[];
+	//Using of loop
+	for (let i=0;i<props.task.length;i++)
+	{
+		let desc = props.task[i].description;
+		let stat =props.task[i].completed;
+		taskarray.push(<Task description={desc} completed={stat}/>)
+	}
+
 	return (
 		<div>
 			<h1>{ props.heading }</h1>
 			<ul>
-				<Task description='Learn React fast'/>
-				<Task description='Learn JSX'/>
-				<Task description='Build a new React app fast'/>
-		                                                    		<li>Build a React App</li>
+				{taskarray}
 			</ul>
 		</div>
 	);
