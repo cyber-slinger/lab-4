fb8d92289cd4ee4cbf97d78b606f90c1b5475177
cyber-slinger <sndpsbd@gmail.com>
2024-10-04 08:50:12 +1000

    Create my own component called Task

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
     return (
         <div className='page'>     
-            <List heading='My Tasks'/>
+            <List heading='My Task 1'/>
         </div>
     );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,24 @@
+function Task(props){
+	return(
+		<li>{props.description}</li>
+
+	);
+}
+
 function List(props) {
 	return (
 		<div>
 			<h1>{ props.heading }</h1>
 			<ul>
-				<li>Learn React</li>
-				<li>Learn JSX</li>
-				<li>Build a React App</li>
+				<Task description='Learn React fast'/>
+				<Task description='Learn JSX'/>
+				<Task description='Build a new React app fast'/>
+		                                                    		<li>Build a React App</li>
 			</ul>
 		</div>
 	);
 }
+
+
 
 export default List;
